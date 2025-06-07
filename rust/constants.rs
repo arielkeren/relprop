8ee8c6ef850eac4a1fb4f772afc2942f0e520930
shmil123 <arielkeren2007@gmail.com>
2025-06-08 00:22:00 +0300

    Allow for set size 0 in the Rust implementation

--- rust/constants.rs
+++ rust/constants.rs
@@ -1,7 +1,7 @@
 pub const NUMBER_OF_PROPERTIES: usize = 17;
-pub const DEFAULT_MIN_SET_SIZE: usize = 1;
+pub const DEFAULT_MIN_SET_SIZE: usize = 0;
 pub const DEFAULT_MAX_SET_SIZE: usize = 5;
-pub const MIN_SET_SIZE: usize = 1;
+pub const MIN_SET_SIZE: usize = 0;
 pub const MAX_SET_SIZE: usize = 8;
 pub const PROPERTY_NAMES: [&str; NUMBER_OF_PROPERTIES] = [
     "Antisymmetry",
