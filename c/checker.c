30411ada081f9208f91c4271d3c71e7401ccd7c4
shmil123 <arielkeren2007@gmail.com>
2025-06-08 16:34:07 +0300

    The C implementation calculates the total relations only once per iteration now

--- c/checker.c
+++ c/checker.c
@@ -17,7 +17,7 @@ void start_checking(const char filename[], uint8_t min_set_size,
 
         clock_t start = clock();
 
-        check_relations(set_size, count);
+        check_relations(set_size, total_relations, count);
 
         clock_t end = clock();
         float elapsed = (float)(end - start) / CLOCKS_PER_SEC;
@@ -28,16 +28,15 @@ void start_checking(const char filename[], uint8_t min_set_size,
     }
 }
 
-void check_relations(uint8_t set_size, uint64_t count[NUMBER_OF_PROPERTIES]) {
-    uint64_t total = 1ULL << (set_size * set_size);
-
+void check_relations(uint8_t set_size, uint64_t total_relations,
+                     uint64_t count[NUMBER_OF_PROPERTIES]) {
 #pragma omp parallel
     {
         uint64_t thread_counts[NUMBER_OF_PROPERTIES] = {0};
         uint8_t relation[MAX_SET_SIZE];
 
 #pragma omp for schedule(static)
-        for (uint64_t idx = 0; idx < total; idx++) {
+        for (uint64_t idx = 0; idx < total_relations; idx++) {
             uint64_t val = idx;
 
             for (uint8_t i = 0; i < set_size; i++) {
--- c/checker.h
+++ c/checker.h
@@ -8,6 +8,7 @@
 void start_checking(const char filename[], uint8_t min_set_size,
                     uint8_t max_set_size);
 
-void check_relations(uint8_t set_size, uint64_t count[NUMBER_OF_PROPERTIES]);
+void check_relations(uint8_t set_size, uint64_t total_relations,
+                     uint64_t count[NUMBER_OF_PROPERTIES]);
 
 #endif
