e29b6b2d79182ea9a0c71311fc9b13f2203228a9
shmil123 <arielkeren2007@gmail.com>
2025-06-14 00:29:24 +0300

    Print the usage more clearly in C

--- highly_optimized/include/parser.h
+++ highly_optimized/include/parser.h
@@ -3,8 +3,8 @@
 
 #include <stdint.h>
 
-uint8_t parse_uint8(const char arg[]);
-
 void get_set_size(int argc, char *argv[], uint8_t *min_size, uint8_t *max_size);
+void print_usage(const char program_name[]);
+uint8_t parse_uint8(const char arg[]);
 
 #endif
--- highly_optimized/src/parser.c
+++ highly_optimized/src/parser.c
@@ -8,19 +8,6 @@
 
 #include "constants.h"
 
-uint8_t parse_uint8(const char arg[]) {
-    errno = 0;
-    char *end;
-    unsigned long val = strtoul(arg, &end, 10);
-
-    if (errno != 0 || *end != '\0' || val > UINT8_MAX) {
-        fprintf(stderr, "Invalid set size value: '%s'\n", arg);
-        exit(1);
-    }
-
-    return (uint8_t)val;
-}
-
 void get_set_size(int argc, char *argv[], uint8_t *min_size,
                   uint8_t *max_size) {
     *min_size = DEFAULT_MIN_SET_SIZE;
@@ -37,9 +24,7 @@ void get_set_size(int argc, char *argv[], uint8_t *min_size,
             *max_size = parse_uint8(argv[i + 1]);
             i++;
         } else {
-            fprintf(stderr, "Unknown or incomplete argument: %s\n", argv[i]);
-            fprintf(stderr, "Usage: %s --min 2 --max 5\n", argv[0]);
-            fprintf(stderr, "Or:    %s -m 2 -M 5\n", argv[0]);
+            print_usage(argv[0]);
             exit(1);
         }
     }
@@ -56,4 +41,32 @@ void get_set_size(int argc, char *argv[], uint8_t *min_size,
                 *min_size, *max_size);
         exit(1);
     }
+}
+
+void print_usage(const char program_name[]) {
+    fprintf(stderr, "Usage: %s [--min <min_size>] [--max <max_size>]\n",
+            program_name);
+
+    fprintf(stderr, "Short form: %s [-m <min_size>] [-M <max_size>]\n",
+            program_name);
+
+    fprintf(stderr,
+            "Minimum set size defaults to %d, and maximum set size defaults to "
+            "%d\n",
+            DEFAULT_MIN_SET_SIZE, DEFAULT_MAX_SET_SIZE);
+
+    fprintf(stderr, "Set sizes have to be between 0 and %d\n", MAX_SET_SIZE);
+}
+
+uint8_t parse_uint8(const char arg[]) {
+    errno = 0;
+    char *end;
+    unsigned long val = strtoul(arg, &end, 10);
+
+    if (errno != 0 || *end != '\0' || val > UINT8_MAX) {
+        fprintf(stderr, "Invalid set size value: '%s'\n", arg);
+        exit(1);
+    }
+
+    return (uint8_t)val;
 }
